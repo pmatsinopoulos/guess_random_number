a749a924a2f9ca960b458ef204986331a520acaf
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2015-11-09 23:12:51 +0200

    First Commit

---
+++ GuessRandomNumber/main.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  GuessRandomNumber
+//
+//  Created by Panayotis Matsinopoulos on 11/9/15.
+//  Copyright © 2015 Panayotis Matsinopoulos. All rights reserved.
+//
+
+import Foundation
+
+var randomNumber = 1
+var userGuess = 1
+var continueGuessing = true
+var keepPlaying = true
+var input = ""
+
+while (keepPlaying) {
+  randomNumber = Int(arc4random_uniform(101)) // get a random number between 0 - 100
+  while (continueGuessing) {
+    print("Pick up a number between 0 and 100")
+    input = NSString(data: NSFileHandle.fileHandleWithStandardInput().availableData, encoding: NSUTF8StringEncoding)! as String
+    input = input.stringByReplacingOccurrencesOfString("\n", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+    userGuess = Int(input)!
+    print("User Guess: \(userGuess)")
+    if (userGuess == randomNumber) {
+      continueGuessing = false
+      print("Congrats!")
+    }
+    else if (userGuess < randomNumber) {
+      print("You have guessed too low")
+    }
+    else {
+      print("You have guessed too high")
+    }
+  }
+  print("Play Again ? Y or N")
+  input = NSString(data: NSFileHandle.fileHandleWithStandardInput().availableData, encoding: NSUTF8StringEncoding)! as String
+  input = input.stringByReplacingOccurrencesOfString("\n", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+  if (input == "N" || input == "n") {
+    keepPlaying = false
+  }
+  continueGuessing = true
+}
+
